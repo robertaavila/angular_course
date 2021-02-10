32faffb8a3339033f739abeb1f6d28a28e359205
USERSAD\rvieirav <rvieirav@everis.com>
2021-02-10 09:58:51 -0300

    Inserting buttons clear and delete functions.

--- src/app/shopping-list/shopping.service.ts
+++ src/app/shopping-list/shopping.service.ts
@@ -2,7 +2,8 @@ import { Subject } from "rxjs";
 import { Ingredient } from "../shared/ingredient.model";
 
   export class ShoppingService {
-    ingredientsChanged = new Subject<Ingredient[]>();  
+    ingredientsChanged = new Subject<Ingredient[]>();
+    startedEditing = new Subject<number>();  
     private ingredients: Ingredient[] = [
     new Ingredient('Apples', 5),
     new Ingredient('Tomatoes', 10),
@@ -10,6 +11,10 @@ import { Ingredient } from "../shared/ingredient.model";
 
   getIngredients() {
     return this.ingredients.slice();
+  }
+
+  getIngredient(index: number) {
+    return this.ingredients[index];
   }
 
   addIngredient(ingredient: Ingredient) {
@@ -24,5 +29,15 @@ import { Ingredient } from "../shared/ingredient.model";
     this.ingredients.push(...ingredient);
     this.ingredientsChanged.next(this.ingredients.slice());
   }
+
+  updateIngredient(index: number, newIngredient: Ingredient) {
+    this.ingredients[index] = newIngredient;
+    this.ingredientsChanged.next(this.ingredients.slice());
+  }
+
+  deleteIngredient(index: number) {
+    this.ingredients.splice(index, 1);
+    this.ingredientsChanged.next(this.ingredients.slice());
+    }
 
 }
