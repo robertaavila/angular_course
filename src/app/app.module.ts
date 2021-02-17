6784eb82a57b66deda9bbdc78c88ea1da69661d4
USERSAD\rvieirav <rvieirav@everis.com>
2021-02-17 16:41:35 -0300

    adding http client to save and fetch data from firebase.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-
+import { HttpClientModule } from '@angular/common/http'
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -17,6 +17,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
 import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 import { RecipeService } from './recipes/recipe.service';
+import { fromEventPattern } from 'rxjs';
 
 @NgModule({
   declarations: [
@@ -36,7 +37,8 @@ import { RecipeService } from './recipes/recipe.service';
     BrowserModule,
     FormsModule,
     AppRoutingModule, 
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [ShoppingService, RecipeService],
   bootstrap: [AppComponent]
---
+++ src/app/shared/data-storage.service.ts
@@ -0,0 +1,38 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { map } from "rxjs/operators"
+
+import { Recipe } from "../recipes/recipe.model";
+import { RecipeService } from "../recipes/recipe.service";
+import { Ingredient } from "./ingredient.model";
+
+
+@Injectable({providedIn: 'root'})
+export class DataStorageService {
+    constructor( private http: HttpClient, private recipeService: RecipeService) {}
+        
+    storeRecipes(){
+       const recipes = this.recipeService.getRecipes();
+       this.http.put(
+           'https://recipe-book-614a4-default-rtdb.firebaseio.com/recipes.json', 
+           recipes
+           )
+           .subscribe(response => {
+            console.log(response);
+       });
+    }
+
+    fetchRecipes() {
+        this.http
+        .get<Recipe[]>('https://recipe-book-614a4-default-rtdb.firebaseio.com/recipes.json')
+        .pipe(map(recipes => {
+            return recipes.map(recipe => {
+                return {...recipe, ingredients: recipe.ingredients ? recipe.ingredients : []}
+            });
+        }))
+        .subscribe(recipes => {
+            this.recipeService.setRecipes(recipes);
+        });
+    }
+    
+}
